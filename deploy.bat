f06d3fb1abbef6e1ded80923237c992014caef8e
zhacai-git <2673411138@qq.com>
2022-10-01 22:35:33 +0800

    Website Updated

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,9 @@
 @echo off
-set day = %date:~0,2%
-set month = %date:~3,2%
-set year = %date:~6,4%
-set hour = %time:~0,2%
-set minute = %time:~3,2%
-echo %day%
-echo Website Update At %year%-%month%-%day% %hour%:%minute% Started
-hexo g
-@echo on
-echo "Generate done."
-git add .
-git commit -m "Website Updated At %year%-%month%-%day% %hour%:%minute%"
-git push
+echo Website Update Started
+git add . 
+"C:\Program Files\Git\cmd\git.exe" commit -m "Website Updated"
+"C:\Program Files\Git\cmd\git.exe" push
+
+echo "Update done."
+
+pause
