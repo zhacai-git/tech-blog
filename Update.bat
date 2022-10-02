e54ec869b365c5a846d6182085d5d849fb35d399
zhacai-git <2673411138@qq.com>
2022-10-02 10:58:16 +0800

    Website Update

--- Update.bat
+++ Update.bat
@@ -1,15 +1,44 @@
 @echo off
+Setlocal ENABLEDELAYEDEXPANSION
+echo -----Website Update Started-----
+echo ACTION: Generating static files.
 call hexo g
-@REM @echo off
-echo Website Update Started
-echo Sync from remote...
-git pull
-echo done.
+echo ACTION: Generate done.
+echo ACTION: Synchronizing from remote, it may takes time depending on your network...
+for /f "tokens=*" %%i in ('git pull') do (
+  set vars = %%i
+  echo %%i
+)
+if "%vars:~-5%" == "date." (
+  echo ACTION: Sync done.
+  goto :addcommit
+) else (
+  goto :SyncFail
+)
+
+:addcommit
 git add . 
-echo added files.
+echo ACTION: added files.
 git commit -m "Website Update"
-git push
+echo ACTION: Commit Done.
+goto :GitPush
 
-echo Update done.
+:SyncFail
+echo ERROR: Git reported an error during pull action, please check the git output for more information.
+echo ERROR: Pull failed, try to continue execution.
+goto :addcommit
+exit 1
 
-pause 
+:GitPush
+echo ACTION: Pushing to remote, it may takes time depending on your network....
+for /f "tokens=*" %%i in ('git push') do (
+  set pushV = %%i
+)
+if "%pushV:~-5%" == "master" (
+  echo ACTION: Push Success, Job done.
+  echo -----Website Update Done-----
+) else (
+  echo ERROR: Push Failed, Website Update NOT DONE. Please check the git output for more information and rerun this script.
+  @REM echo Git Opt:%pushV%
+)
+pause
