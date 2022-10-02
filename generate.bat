60a45974bda83ddeb57f899e336c29479238f149
zhacai-git <2673411138@qq.com>
2022-10-02 10:47:28 +0800

    Website Update

--- generate.bat
+++ generate.bat
@@ -1,8 +1,10 @@
 @echo off
 Setlocal ENABLEDELAYEDEXPANSION
+echo -----Website Update Started-----
+echo ACTION: Generating static files.
 call hexo g
-echo Website Update Started
-echo Synchronizing from remote, it may takes time depending on your network...
+echo ACTION: Generate done.
+echo ACTION: Synchronizing from remote, it may takes time depending on your network...
 for /f "tokens=*" %%i in ('git pull') do (
   set vars = %%i
 )
@@ -15,30 +17,31 @@ if defined vars (
 )
 
 :addcommit
-echo Sync done.
+echo ACTION: Sync done.
 git add . 
-echo added files.
+echo ACTION: added files.
 git commit -m "Website Update"
-echo Commit Done.
+echo ACTION: Commit Done.
 goto :GitPush
 
 :SyncFail
-echo Git reported an error during pull action, please check the git output for more information.
+echo ERROR: Git reported an error during pull action, please check the git output for more information.
 echo Git Opt:%vars%
 pause
 exit 1
 
 :GitPush
-echo Pushing to remote.
+echo ACTION: Pushing to remote, it may takes time depending on your network....
 for /f "tokens=*" %%i in ('git push') do (
   set pushV = %%i
 )
 set result = %pushV:~-11%
 if "%result:~0,5%" == "errno" (
-  echo Push Failed, please check for git output.
+  echo ERROR: Push Failed, please check for git output.
   echo Git Opt:%pushV%
 ) else (
-  echo "Push Success, Job done."
+  echo ACTION: Push Success, Job done.
+  echo -----Website Update Done-----
 )
 
 pause
