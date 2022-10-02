6e1ba380b7bcc55bb15858604cc9f77d7c8431e9
zhacai-git <2673411138@qq.com>
2022-10-02 10:30:22 +0800

    Website Update

--- generate.bat
+++ generate.bat
@@ -3,13 +3,35 @@ call hexo g
 @REM @echo off
 echo Website Update Started
 echo Sync from remote...
-git pull
+for /f "tokens=*" %%i in ('git pull') do (
+  set vars = %%i
+)
+if defined vars (
+  if "%vars%" == "Already up to date." (
+    goto :addcommit
+  ) else (
+    goto :SyncFail
+  )
+)
+
+:addcommit
 echo done.
 git add . 
 echo added files.
 git commit -m "Website Update"
-git push
-echo %errorlevel%
-echo Update done.
+echo Commit Done.
+goto :GitPush
+
+:SyncFail
+echo Git reported an error during pull action, please check the internet connection.
+pause
+exit 1
+
+:GitPush
+echo Pushing to remote.
+for /f "tokens=*" %%i in ('git push') do (
+  set pushV = %%i
+)
+echo %pushV%
 
 pause
