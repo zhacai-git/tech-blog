05a5e2b0aaeddea1c9a75dbd6b826f83a0066217
zhacai-git <2673411138@qq.com>
2022-10-02 11:16:16 +0800

    Website Update

--- Update.bat
+++ Update.bat
@@ -8,7 +8,7 @@ echo ACTION: Generate done.
 echo ACTION: Synchronizing from remote, it may takes time depending on your network...
 for /f "tokens=*" %%i in ('git pull') do (
   SET pullV = %%i
-  echo %%i
+  echo %%i:~1,5
   echo cutted: %pullV%
   if "%%%i:~-5%" == "date." (
     echo ACTION: Sync done.
