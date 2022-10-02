30a2ff0fdd42f22f6881ae2839eefc2599924228
zhacai-git <2673411138@qq.com>
2022-10-02 11:05:01 +0800

    Website Update

--- Update.bat
+++ Update.bat
@@ -8,7 +8,7 @@ echo ACTION: Synchronizing from remote, it may takes time depending on your netw
 for /f "tokens=*" %%i in ('git pull') do (
   set vars = %%i
   echo %%i
-  echo %vars%
+  echo %%vars
 )
 echo %vars:~-5%
 if "%vars:~-5%" == "date." (
